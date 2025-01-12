fcaac2c7b23e44eac645c021d9b1204551b6f1da
Philippmer <philipp@meran-online.de>
2025-01-12 12:30:14 +0100

    allPairs completed

--- src/INES.py
+++ src/INES.py
@@ -1,9 +1,11 @@
 from Node import Node
 from network import generate_eventrates, create_random_tree
 from graph import create_fog_graph
+from graph import draw_graph
+from allPairs import populate_allPairs
 
 class INES():
-    allPais: int
+    allPais: list
     network: list[Node]
     eventrates: list[list[int]]
 
@@ -14,10 +16,11 @@ class INES():
         self.primitiveEvents= self.eventrates[0]
         root, self.network = create_random_tree(nwSize,self.eventrates,node_event_ratio,max_partens) 
         self.graph = create_fog_graph(self.network)
+        self.allPais = populate_allPairs(self.graph)
 
-from graph import draw_graph
 my_ines = INES(50,0.5,6,0.3,2)
 print(my_ines.network)
+print(my_ines.allPais)
 draw_graph(my_ines.graph)
 
 for i in my_ines.network:
