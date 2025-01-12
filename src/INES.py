fe521500ba181c5958142199542f865eb075cb81
Philippmer <philipp@meran-online.de>
2025-01-12 13:11:42 +0100

    finished queryworkload

--- src/INES.py
+++ src/INES.py
@@ -1,27 +1,29 @@
 from Node import Node
-from network import generate_eventrates, create_random_tree
+from network import generate_eventrates, create_random_tree,generate_events
 from graph import create_fog_graph
 from graph import draw_graph
 from allPairs import populate_allPairs
+from queryworkload import generate_workload
 
 class INES():
     allPais: list
     network: list[Node]
     eventrates: list[list[int]]
-
+    query_workload = None
 
     primitiveEvents: list[int]
-    def __init__(self, nwSize: int, node_event_ratio: float, num_eventtypes: int, eventskew: float, max_partens: int):
+    def __init__(self, nwSize: int, node_event_ratio: float, num_eventtypes: int, eventskew: float, max_partens: int, query_size: int, query_length:int):
         self.eventrates = generate_eventrates(eventskew,num_eventtypes)
-        self.primitiveEvents= self.eventrates[0]
+        self.primitiveEvents= generate_events(self.eventrates,node_event_ratio)
         root, self.network = create_random_tree(nwSize,self.eventrates,node_event_ratio,max_partens) 
         self.graph = create_fog_graph(self.network)
         self.allPais = populate_allPairs(self.graph)
+        self.query_workload = generate_workload(query_size,query_length,self.primitiveEvents)
 
-my_ines = INES(50,0.5,6,0.3,2)
-print(my_ines.network)
-print(my_ines.allPais)
-draw_graph(my_ines.graph)
+my_ines = INES(50,0.5,6,0.3,2,3,5)
+print(my_ines.query_workload)
+#print(my_ines.allPais)
+#draw_graph(my_ines.graph)
 
-for i in my_ines.network:
-    print(i)
+# for i in my_ines.network:
+#     print(i)
---
+++ src/helper/parse_network.py
@@ -0,0 +1,90 @@
+import string
+
+# Private global variables
+_network = None
+_rates = None
+_primEvents = None
+_instances = None
+_nodes = None
+
+# Function to initialize global variables
+def initialize_globals(network_data):
+    global _network, _rates, _primEvents, _instances, _nodes
+    
+    # Initialize global variables
+    _network = {}
+    _rates = {}
+    _primEvents = []
+    _instances = {}
+    _nodes = {}
+
+    ind = 0
+    for node in network_data:
+        _network[ind] = []
+        for eventtype in range(len(node.eventrates)):
+            if node.eventrates[eventtype] > 0:
+                event = string.ascii_uppercase[eventtype]
+                _network[ind].append(event)
+                if event not in _primEvents:
+                    _primEvents.append(event)
+                if event not in _rates:
+                    _rates[event] = float(node.eventrates[eventtype])
+        ind += 1
+
+    # Populate nodes dictionary
+    for i in _network.keys():
+        for event in _network[i]:
+            if event not in _nodes:
+                _nodes[event] = [i]
+            else:
+                _nodes[event].append(i)
+
+    # Populate instances dictionary
+    for i in _primEvents:
+        _instances[i] = len(_nodes[i])
+
+# Getter functions with lazy initialization
+def get_network(network_data):
+    if _network is None:
+        initialize_globals(network_data)
+    return _network
+
+def get_rates(network_data):
+    if _rates is None:
+        initialize_globals(network_data)
+    return _rates
+
+def get_primEvents(network_data):
+    if _primEvents is None:
+        initialize_globals(network_data)
+    return _primEvents
+
+def get_instances(network_data):
+    if _instances is None:
+        initialize_globals(network_data)
+    return _instances
+
+def get_nodes(network_data):
+    if _nodes is None:
+        initialize_globals(network_data)
+    return _nodes
+
+# Utility function for accessing network events
+def events(node, network_data):
+    if _network is None:
+        initialize_globals(network_data)
+    return _network[node]
+
+# Function to calculate instances from projections
+def instances_func(proj, network_data):    
+    if _nodes is None:
+        initialize_globals(network_data)
+    
+    num = 1
+    for i in proj:
+        if len(i) == 1:
+            num *= len(_nodes[i])
+        else:
+            for j in i:
+                num *= len(_nodes[j])
+    return num
