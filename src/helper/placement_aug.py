03c021a263c7b3a7c37b66390a36d9ef68b158c3
Philippmer <philipp@meran-online.de>
2025-01-17 08:28:20 +0100

    started Operator Placement

---
+++ src/helper/placement_aug.py
@@ -0,0 +1,280 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 10 13:16:11 2021
+
+@author: samira
+"""
+import multiprocessing
+from helper.processCombination_aug import *
+from functools import partial
+from allPairs import find_shortest_path_or_ancestor
+
+
+def NEWcomputeMSplacementCosts(projection, sourcetypes, destinationtypes, noFilter): #we need tuples, (C, [E,A]) C should be sent to all e and a nodes ([D,E], [A]) d and e should be sent to all a nodes etc
+    #print(projection, sourcetypes)
+
+    
+    costs = 0
+    destinationNodes = []     
+
+    for etype in destinationtypes:
+        for etb in IndexEventNodes[etype]:
+            destinationNodes += getNodes(etb)
+            
+            
+    newInstances = [] #!
+    pathLength = 0        
+    etype = sourcetypes[0]
+    
+    f = open("msFilter.txt", "w")
+    if etype in projFilterDict.keys() and getMaximalFilter(projFilterDict, etype, noFilter):
+        print("hasFilter")
+        f.write("VAR=true")
+    else:        
+        f.write("VAR=false")
+    f.close()     
+        
+        
+    for etb in IndexEventNodes[etype]: #parallelize 
+            
+       
+            MydestinationNodes = list(set(destinationNodes).difference(set(getNodes(etb)))) #only consider nodes that do not already hold etb
+            if MydestinationNodes: #are there ms nodes which did not receive etb before
+                    node = findBestSource(getNodes(etb), MydestinationNodes) #best source is node closest to a node of destinationNodes
+                    treenodes = copy.deepcopy(MydestinationNodes) 
+                    treenodes.append(node)
+                    
+                    mytree = steiner_tree(G, treenodes)
+                    
+                    myInstance = Instance(etype, etb, [node], {projection: list(mytree.edges)}) #! #append routing tree information for instance/etb                    
+                    newInstances.append(myInstance) #!
+                    
+                    
+                    myPathLength = max([len(nx.shortest_path(mytree, x, node, method='dijkstra')) for x in MydestinationNodes]) - 1
+                    
+                    
+                    if etype in projFilterDict.keys() and  getMaximalFilter(projFilterDict, etype, noFilter): #case input projection has filter
+                        mycosts =  len(mytree.edges()) * getDecomposedTotal(getMaximalFilter(projFilterDict, etype, noFilter), etype)                    
+                        if len(IndexEventNodes[etype]) > 1 : # filtered projection has ms placement
+                             partType = returnPartitioning(etype, mycombi[etype])[0]                     
+                             mycosts -= len(mytree.edges())  * rates[partType] * singleSelectivities[getKeySingleSelect(partType, etype)] * len(IndexEventNodes[etype])
+                             mycosts += len(mytree.edges())  * rates[partType] * singleSelectivities[getKeySingleSelect(partType, etype)] 
+                    elif len(etype) == 1:
+                        mycosts = len(mytree.edges()) * rates[etype]
+                    else:                    
+                        num = NumETBsByKey(etb, etype)                 
+                        mycosts = len(mytree.edges()) *  projrates[etype][1] * num     # FILTER              
+                        
+                    placementTreeDict[(tuple(destinationtypes),etb)] = [node, MydestinationNodes, mytree] #only kept for updating in the next step
+                    costs +=  mycosts 
+                    
+                    pathLength = max([pathLength, myPathLength])
+                    
+                    # update events sent over network
+                    for routingNode in mytree.nodes():
+                        if not routingNode in getNodes(etb):
+                            setEventNodes(routingNode, etb)
+    
+        
+  # print(list(map(lambda x: str(x), sourcetypes)), costs)           
+    return costs, pathLength, newInstances
+
+
+
+def findBestSource(sources, actualDestNodes): #this is only a heuristic, as the closest node can still be shit with respect to a good steiner tree ?+
+    curmin = np.inf     
+    for node in sources:        
+        if min([allPairs[node][x] for x in actualDestNodes]) < curmin:
+           curmin =  min([allPairs[node][x] for x in actualDestNodes])
+           bestSource = node
+    return bestSource
+        
+def getDestinationsUpstream(projection):
+    return  range(len(allPairs))    
+    myPartTypes = []
+    nodes = []
+    for i in mycombi.keys():
+        if projection in mycombi[i]:
+            partType = originalDict[i][1]
+            if partType:
+                myPartTypes.append(partType[0])
+    if myPartTypes:
+        for k in myPartTypes:
+            for etb in IndexEventNodes[k]:
+                nodes.append(getNodes(etb)[0])
+        
+        return nodes
+    else:        
+        return  range(len(allPairs))      
+        
+def ComputeSingleSinkPlacement(projection, combination, noFilter):
+    from allPairs import create_routing_dict
+    routingDict = create_routing_dict()
+    costs = np.inf
+    node = 0
+    Filters  = []    
+    
+     
+    # add filters of projections to eventtpes in combi, if filters added, use costs of filter -> compute costs for single etbs of projrates 
+    intercombi = []
+    ##### FILTERS
+    for proj in combination:
+        intercombi.append(proj)  
+        #print(list(map(lambda x: str(x), list(projFilterDict.keys()))))
+        if len(proj) > 1 and len(getMaximalFilter(projFilterDict, proj, noFilter)) > 0:            
+            Filters.append((proj,getMaximalFilter(projFilterDict, proj, noFilter)))            
+            for etype in getMaximalFilter(projFilterDict, proj, noFilter):
+                intercombi.append(etype)
+    combination = list(set(intercombi))
+    
+    myProjection = Projection(projection, {}, [], [], Filters) #!
+    
+
+    # Extract only the keys (nodes) with an empty list of connections
+    non_leaf = [node for node, neighbors in network.items() if not neighbors]
+    
+    for destination in non_leaf:
+        skip_destination = False  # Flag to determine if we should skip this destination
+        for eventtype in combination:
+            for etb in IndexEventNodes[eventtype]:
+                possibleSources = getNodes(etb)
+                for source in possibleSources:
+                    # Use the routing_dict to get the common ancestor
+                    common_ancestor = routingDict[destination][source]['common_ancestor']
+                    if common_ancestor != destination:
+                        # print(f"Skipping destination {destination} for source {source} (Common ancestor: {common_ancestor})")
+                        skip_destination = True
+                        break
+                if skip_destination:
+                    break  # Break out of the etb loop
+            if skip_destination:
+                break  # Break out of the eventtype loop
+        if skip_destination:
+            continue  # Move on to the next destination without computing costs
+        
+        mycosts = 0
+        for eventtype in combination:
+
+                for etb in IndexEventNodes[eventtype]: #check for all sources #here iterated over length of IndesEventNodes to get all sources for etb Instances
+                        
+                        possibleSources = getNodes(etb)
+                        mySource = possibleSources[0]
+                        for source in possibleSources:
+                            if allPairs[destination][source] < allPairs[destination][mySource]:                               
+                                   mySource  = source
+                        
+                        #print(hops)
+                        if eventtype in projFilterDict.keys() and  getMaximalFilter(projFilterDict, eventtype, noFilter): #case filter 
+                            mycosts +=  allPairs[destination][mySource] * getDecomposedTotal(getMaximalFilter(projFilterDict, eventtype, noFilter), eventtype)                    
+                            if len(IndexEventNodes[eventtype]) > 1 : # filtered projection has ms placement
+                                partType = returnPartitioning(eventtype, mycombi[eventtype])[0]                     
+                                mycosts -= allPairs[destination][mySource] * rates[partType] * singleSelectivities[getKeySingleSelect(partType, eventtype)] * len(IndexEventNodes[eventtype])
+                                mycosts += allPairs[destination][mySource] * rates[partType] * singleSelectivities[getKeySingleSelect(partType, eventtype)]
+                        elif eventtype in rates.keys():        # case primitive event
+
+                            mycosts += (rates[eventtype] * allPairs[destination][mySource] )
+                        else: # case projection                         
+                             num = NumETBsByKey(etb, eventtype)
+                             mycosts += projrates[eventtype][1] * allPairs[destination][mySource] * num 
+        print(mycosts)
+        if mycosts < costs:
+            costs = mycosts
+            node = destination
+    myProjection.addSinks(node) #!
+    newInstances = [] #!
+    # Update Event Node Matrice, by adding events etbs sent to node through node x to events of node x
+    longestPath  = 0
+    for eventtype in combination:
+            curInstances = [] #!
+            for etb in IndexEventNodes[eventtype]:
+                possibleSources = getNodes(etb)
+                mySource = possibleSources[0] #??
+                for source in possibleSources:                    
+                    if allPairs[node][source] < allPairs[node][mySource]:
+                       mySource  = source     
+
+                shortestPath = find_shortest_path_or_ancestor(G, mySource, node) 
+              
+                if len(shortestPath) - 1 > longestPath:
+                    longestPath = len(shortestPath) - 1                    
+                newInstance = Instance(eventtype, etb, [mySource], {projection: shortestPath}) #!
+                curInstances.append(newInstance) #!    
+                
+                for stop in shortestPath:
+                    if not stop in getNodes(etb):                        
+                        setEventNodes(stop, etb) 
+                        
+            newInstances += curInstances          #!   
+            myProjection.addInstances(eventtype, curInstances)     #!        
+                        
+    SiSManageETBs(projection, node)
+    hops = len(find_shortest_path_or_ancestor(G, 0, node)) - 1 if len(find_shortest_path_or_ancestor(G, 0, node)) > 1 else 0
+    myProjection.addSpawned([IndexEventNodes[projection][0]]) #!
+    costs += hops
+    return costs, node, longestPath, myProjection, newInstances, Filters
+
+def costsAt(eventtype, node):
+    mycosts = 0
+    for etb in IndexEventNodes[eventtype]:
+                possibleSources = getNodes(etb)
+                mySource = possibleSources[0]
+                for source in possibleSources:
+                    if allPairs[node][source] <= allPairs[node][mySource]:
+                       mySource  = source
+                mycosts += rates[eventtype] * allPairs[node][mySource] 
+    return mycosts
+
+def NEWcomputeCentralCosts(workload):
+    #Adding all Eventtypes (simple events) to the list
+    eventtypes = []
+    for i in workload:
+        myevents = i.leafs()
+        #print(myevents)
+        for e in myevents:
+            # print(e)
+            # print("Eventtypes")
+            # print(eventtypes)
+            if not e in eventtypes:
+                eventtypes.append(e)
+    #print(eventtypes)
+    costs = np.inf
+    node = 0
+    # for destination in range(len(allPairs)):
+    destination = 0
+    mycosts = 0      
+    for eventtype in eventtypes:
+        oldcosts = mycosts
+        for etb in IndexEventNodes[eventtype]:
+
+            possibleSources = getNodes(etb)
+            mySource = possibleSources[0]
+            for source in possibleSources:
+                
+                if allPairs[destination][source] <= allPairs[destination][mySource]:
+                    mySource  = source
+            mycosts += rates[eventtype] * allPairs[destination][mySource]       
+    if mycosts < costs:
+        costs = mycosts
+        node = destination
+    longestPath = max(allPairs[node])
+    
+    routingDict = {} # for evaluation plan
+    for e in eventtypes:        
+        routingDict[e] = {}
+        for etb in IndexEventNodes[e]:
+            possibleSources = getNodes(etb)
+            mySource = possibleSources[0]
+            shortestPath = nx.shortest_path(G, mySource, node, method='dijkstra')  
+            routingDict[e][etb] = shortestPath
+ 
+    for eventtype in eventtypes:    
+        thiscosts = 0
+        for etb in IndexEventNodes[eventtype]:
+                    mySource = getNodes(etb)[0]         
+                    thiscosts += rates[eventtype] * allPairs[node][mySource]
+                  #  print(allPairs[node][mySource])
+      #  print(eventtype, thiscosts )    
+    return (costs, node, longestPath, routingDict) 
+                    
+#TODO compute and print rates saved by placement
---
+++ src/operatorplacement.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 18 16:32:53 2021
+
+@author: samira
+"""
+from helper.placement_aug import *
+import time
+import csv
+import sys
+import argparse
+
+maxDist = max([max(x) for x in allPairs])
+
+def getLowerBound(query): # lower bound -> for multiple projections, keep track of events sent as single sink and do not add up
+    MS = []
+    for e in query.leafs():        
+        myprojs= [p for p in list(set(projsPerQuery[query]).difference(set([query]))) if totalRate(p)<rates[e] and not e in p.leafs()]
+        if myprojs:
+            MS.append(e)
+        for p in [x for x in projsPerQuery[query] if e in x.leafs()]:
+            part = returnPartitioning(p,p.leafs())            
+            if e in part:
+                MS.append(e)
+    nonMS = [e for e in query.leafs() if not e in MS]  
+    if nonMS:          
+        minimalRate = sum(sorted([totalRate(e) for e in query.leafs() if not e in MS])) * longestPath
+    else:
+        minimalRate = min([totalRate(e) for e in query.leafs()]) * longestPath
+    minimalProjs = sorted([totalRate(p) for p in projsPerQuery[query] if not p==query])[:len(list(set(MS)))-1]
+    if not len(nonMS) == len(query.leafs()):
+        minimalRate +=  sum(minimalProjs) * longestPath
+    return minimalRate#, nonMS) 
+
+
+
+def parse_arguments():
+    # Initialize the parser
+    parser = argparse.ArgumentParser(description="Process filename and number of parents.")
+
+    # Add the parameters
+    parser.add_argument('--file', type=str, required=True, default="test", help="Filename to process")
+    parser.add_argument('--number_parents', type=int, default=1, help="Number of parents (default: 1)")
+
+    # Parse the arguments and return the results
+    return parser.parse_args()
+
+
+def main():
+    
+  
+    Filters = []
+    writeExperimentData = 0
+    
+    filename = "results"
+    noFilter = 0 # NO FILTER
+    
+    #only write experiment data for shared costs, use ID and total costs for writing results of shared/seperate window
+    args = parse_arguments()
+
+    # Access the arguments
+    filename = args.file
+    number_parents = args.number_parents
+
+    print(filename)
+    ccosts = NEWcomputeCentralCosts(wl)
+    #print("central costs : " + str(ccosts))
+    centralHopLatency = max(allPairs[ccosts[1]])
+    numberHops = sum(allPairs[ccosts[1]])
+    print("centralCosts: " + str(ccosts[0]))
+    print("Central Hops: " + str(numberHops))
+    print("central Hop Latency: " + str(centralHopLatency))
+    MSPlacements = {}
+    curcosts = 1 
+    start_time = time.time()
+    
+    hopLatency = {}
+    
+    #Reduce calls of initEventNodes
+    init_eventNodes = initEventNodes()   
+    EventNodes = init_eventNodes[0]
+    IndexEventNodes = init_eventNodes[1]    
+    
+    myPlan = EvaluationPlan([], [])
+    
+    #transforming indexeventnodes into EvaluationPLan object with all entries as a instance
+    #jede instance ist eine node ein event (nodes * events die produziert werden pro node)
+    myPlan.initInstances(IndexEventNodes) # init with instances for primitive event types
+    
+    #mycombi = removeSisChains()
+    unfolded = mycombi
+ 
+    dependencies = compute_dependencies(unfolded)
+    processingOrder = sorted(compute_dependencies(unfolded).keys(), key = lambda x : dependencies[x] ) # unfolded enthält kombi   
+    costs = 0
+
+    for projection in processingOrder:  #parallelize computation for all projections at the same level
+            if set(unfolded[projection]) == set(projection.leafs()): #initialize hop latency with maximum of children
+               hopLatency[projection] = 0 
+            else:
+                hopLatency[projection] = max([hopLatency[x] for x in unfolded[projection] if x in hopLatency.keys()])
+
+          
+            partType = returnPartitioning(projection, unfolded[projection], criticalMSTypes)
+
+                
+            result = ComputeSingleSinkPlacement(projection, unfolded[projection], noFilter)
+            additional = result[0]
+            costs += additional
+            hopLatency[projection] += result[2]
+            myPlan.addProjection(result[3]) #!
+            for newin in result[3].spawnedInstances: # add new spawned instances
+                
+                myPlan.addInstances(projection, newin)
+            
+            myPlan.updateInstances(result[4]) #! update instances
+            Filters += result[5]
+            
+            print("SiS " + str(projection) + "PC: " + str(additional)  + " Hops: " + str(result[2]))
+                
+    mycosts = costs/ccosts[0]
+    print("INEv Transmission " + str(costs) )
+    if len(wl)>1 or wl[0].hasKleene() or wl[0].hasNegation():
+        lowerBound = 0
+    else:
+      for query in wl:
+        lowerBound= getLowerBound(query)
+    print("Lower Bound: " + str(lowerBound / ccosts[0]))
+
+    print("Transmission Ratio: " + str(mycosts))
+    print("INEv Depth: " + str(float(max(list(dependencies.values()))+1)/2))
+    
+    ID = int(np.random.uniform(0,10000000))
+    
+    with open('EvaluationPlan',  'wb') as EvaluationPlan_file:
+        pickle.dump([myPlan, ID, MSPlacements], EvaluationPlan_file)
+    
+    totaltime = str(round(time.time() - start_time, 2))
+
+    print("Printing execution times")
+    print(start_time)
+    print(time.time())
+    print("Finished in " + totaltime + " seconds")
+    
+            
+    #getNetworkParameters, selectivityParameters, combigenParameters
+        
+    with open('networkExperimentData', 'rb') as networkExperimentData_file: 
+          networkParams = pickle.load(networkExperimentData_file)   
+    with open('selectivitiesExperimentData', 'rb') as selectivities_file: 
+          selectivityParams  = pickle.load(selectivities_file)   
+    with open('combiExperimentData', 'rb') as combiExperimentData_file: 
+          combigenParams = pickle.load(combiExperimentData_file) 
+    with open('processingLatency', 'rb') as processingLatency_file: 
+          processingLatencyParams = pickle.load(processingLatency_file)            
+                      
+    ID = int(np.random.uniform(0,10000000))
+    hopfactor = processingLatencyParams[2]
+
+   
+    hoplatency = max([hopLatency[x] for x in hopLatency.keys()])   
+    totalLatencyRatio = hoplatency / centralHopLatency
+    myResult = [ID, mycosts, ccosts[0], costs,Filters, networkParams[3], networkParams[0], networkParams[2], len(wl), combigenParams[3], selectivityParams[0], selectivityParams[1], combigenParams[1], longestPath, totaltime, centralHopLatency, float(max(list(dependencies.values()))/2), ccosts[0], lowerBound / ccosts[0], networkParams[1], number_parents]
+    schema = ["ID", "TransmissionRatio", "Transmission","INEvTransmission","FilterUsed", "Nodes", "EventSkew", "EventNodeRatio", "WorkloadSize", "NumberProjections", "MinimalSelectivity", "MedianSelectivity","CombigenComputationTime", "Efficiency", "PlacementComputationTime", "centralHopLatency", "Depth",  "CentralTransmission", "LowerBound", "EventTypes", "MaximumParents"] 
+    
+ 
+    new = False
+    try:
+         f = open("../res/"+str(filename)+".csv")   
+    except FileNotFoundError:
+         new = True           
+        
+    with open("../res/"+str(filename)+".csv", "a") as result:
+       writer = csv.writer(result)  
+       if new:
+           writer.writerow(schema)              
+       writer.writerow(myResult)
+      
+
+    with open('CentralEvaluationPlan',  'wb') as CentralEvaluationPlan_file:
+        pickle.dump([ccosts[1],ccosts[3], wl], CentralEvaluationPlan_file)
+    
+    with open('ExperimentID',  'wb') as ExperimentID_file:
+        pickle.dump([ID,ccosts[0]], ExperimentID_file)
+    
+    if writeExperimentData:
+        with open('ExperimentResults',  'wb') as ExperimentID_file: 
+            pickle.dump([ID,costs], ExperimentID_file)  # write only INEv Costs and ID for timewindow exp
+
+
+if __name__ == "__main__":
+    main()                    
